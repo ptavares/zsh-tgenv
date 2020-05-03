1b03070d94e9e6a529d4372892130772e9b9fa6b
Patrick Tavares <tavarespatrick01@gmail.com>
2020-05-03 10:39:22 +0200

    :tada: tgenv zsh-plugin

---
+++ zsh-tgenv.plugin.zsh
@@ -0,0 +1,21 @@
+GITHUB="https://github.com"
+
+[[ -z "$TGFENV_HOME" ]] && export TGENV_HOME="$HOME/.tgenv"
+
+_zsh_tgenv_install() {
+    echo "Installing tgenv..."
+    git clone "${GITHUB}/cunymatthieu/tgenv.git" "${TGENV_HOME}"
+}
+
+_zsh_tgenv_load() {
+    # export PATH
+    export PATH="$TGENV_HOME/bin:$PATH"
+}
+
+# install tgenv if it isnt already installed
+[[ ! -f "$TGENV_HOME/bin/tgenv" ]] && _zsh_tgenv_install
+
+# load tgenv if it is installed
+if [[ -f "$TGENV_HOME/bin/tgenv" ]]; then
+    _zsh_tgenv_load
+fi
